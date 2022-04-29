cde2b108ce898867db26123e5284c2c167853c90
Mark Murphy <murphy.mark@northeastern.edu>
2022-04-29 15:49:16 -0400

    Move dayjs.extend out of useEffect

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,12 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Home from './Home';
 
 const queryClient = new QueryClient();
+dayjs.extend(relativeTime);
 
 function App() {
-  useEffect(() => {
-    dayjs.extend(relativeTime);
-  }, []);
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className="h-screen w-screen">
