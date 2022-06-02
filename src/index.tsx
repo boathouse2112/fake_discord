7d4135aa8456c8c34c5764542ab738ac77f5c60d
Mark Murphy <boathouse2112@gmail.com>
2022-06-02 06:55:19 -0400

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import './index.css';
 import App from './views/App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +12,8 @@ const root = ReactDOM.createRoot(
 root.render(
   // Disabling strict-mode for firebaseui-web-react, per https://github.com/firebase/firebaseui-web-react/issues/172
   // <React.StrictMode>
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
   // </React.StrictMode>
 );
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,31 +1,33 @@
+import { useAuthUser } from '@react-query-firebase/auth';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ChannelView from '../components/server/ChannelView';
+import { auth } from '../firebase';
 import Home from './Home';
 import Login from './Login';
 import ServerView from './ServerView';
-
-const queryClient = new QueryClient();
 dayjs.extend(relativeTime);
 
 const USER_ID = 'HHpwr6hXRpEg5loOSmWP';
 
 function App() {
+  const { isSuccess: loggedIn } = useAuthUser(['user'], auth);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/@me" element={<Home />} />
-          <Route path="/:serverID" element={<ServerView userID={USER_ID} />}>
-            <Route path=":channelID" element={<ChannelView />} />
-          </Route>
-          <Route path="*" element={<Navigate to="/@me" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={loggedIn ? <Home /> : <Login />} />
+        <Route path="/@me" element={loggedIn ? <Home /> : <Login />} />
+        <Route
+          path="/:serverID"
+          element={loggedIn ? <ServerView userID={USER_ID} /> : <Login />}
+        >
+          <Route path=":channelID" element={<ChannelView />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/@me" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
